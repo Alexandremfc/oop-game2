82168700af1c837c69b99749a28ad8ec6559ce59
Alexandre Cabral <alex.miguel89@gmail.com>
2024-04-19 10:08:11 +0100

    Milestone1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,40 @@
-console.log("hello world");
+
+
+
+class Player {
+    constructor() {
+        this.positionX = 50;
+        this.positionY = 0;
+        this.width = 20;
+        this.height = 20;
+        this.color = "red";
+
+       this.playerElm = document.getElementById("player");
+       this.playerElm.style.bottom = this.positionY + "vh";
+       this.playerElm.style.left = this.positionX + "vw";
+       this.playerElm.style.width = this.width + "vw";
+       this.playerElm.style.height = this.height + "vh";
+       
+        
+    }
+    moveLeft(){
+        this.positionX--;
+        this.playerElm.style.left = this.positionX + "vw";
+    }
+    moveRight() {
+        this.positionX++;
+        this.playerElm.style.left = this.positionX + "vw";
+    } //always .left cause we located on bottom left to begin
+}
+
+
+
+const player = new Player();
+
+document.addEventListener("keydown", (e) => {
+   if (e.code === "ArrowLeft") {
+        player.moveLeft();
+    } else if (e.code === "ArrowRight") {
+        player.moveRight();
+    }
+});
